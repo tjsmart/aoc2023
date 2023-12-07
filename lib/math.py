c4f7a46db0e33c0e763f80c835a19db4ee404752
Tyler Smart <tjsmart@ucsc.edu>
2023-12-06 22:55:37 -0700

    add product helper

--- lib/math.py
+++ lib/math.py
@@ -1,4 +1,7 @@
 import math
+from collections.abc import Iterable
+from typing import overload
+
 
 def solve_quadratic(a: float, b: float, c: float) -> tuple[float, float]:
     return (
@@ -7,6 +10,24 @@ def solve_quadratic(a: float, b: float, c: float) -> tuple[float, float]:
     )
 
 
+@overload
+def product(values: Iterable[int], default: int = 1) -> int:
+    ...
+
+
+@overload
+def product(values: Iterable[float], default: float = 1) -> float:
+    ...
+
+
+def product(values: Iterable[float], default: float = 1) -> float:
+    p = default
+    for value in values:
+        p *= value
+    return p
+
+
 __all__ = [
     "solve_quadratic",
+    "product",
 ]
